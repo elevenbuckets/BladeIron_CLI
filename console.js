b5fc56e82bf9c4bec73eec77beb2ef3bfed95084
jasonlin <jasonlin@elevenbuckets.com>
2019-02-16 10:20:03 -0700

    ensure unsubscribe ipfs pubsub topic when exit

--- console.js
+++ console.js
@@ -214,6 +214,9 @@ if (cluster.isMaster) {
 						r.context = {app};
 					       	r.on('exit', () => {
 					       		console.log("\n\t" + 'Stopping CLI...');
+							app[appName].ipfs_pubsub_topicList.map((t) => {
+								app[appName].ipfs_pubsub_unsubscribe(t);
+							})
 							app[appName].client.close();
 					       	});
 					}
